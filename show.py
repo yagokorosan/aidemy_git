998ed5d466158f83baf42babf29157c17039fecf
yagokorosan <126132372+yagokorosan@users.noreply.github.com>
2023-06-01 18:59:30 +0900

    add today

--- show.py
+++ show.py
@@ -5,4 +5,4 @@ import random
 
 with open('./hyakunin.txt', encoding = 'utf-8') as f:
     wakas =[s.strip() for s in f.readlines()]
-print(wakas[random.randrange(len(wakas))]) 
+print("today's waka" + wakas[random.randrange(len(wakas))]) 
